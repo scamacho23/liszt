c90bd4b9afe31140b230471a35c97342a869a3c6
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-07-30 11:43:03 -0400

    Add archive note to install.py
    Upon installation, a '.archive' note will be added to the user's
    .quicknote file. This is where archived memories will be kept.
    Archiving has not been added yet, but will be soon. This commit
    is in preparation of that

--- install.py
+++ install.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# This file installs the .quicknote
+# folder in the user's home directory,
+# and adds the file_list, quicknote_cache
+# default and archive files to that folder
 
 import sys
 import os.path
@@ -23,24 +27,30 @@ Makes a file to contain the names of all of the users note-files
 """
 def make_background_files():
 	file_list = path.expanduser('~/.quicknote/.filelist')
-	open(file_list, 'w+')
+	with open(file_list, 'w+') as f:
+		f.write('default')
+		f.write('archive')
 	quicknote_cache = path.expanduser('~/.quicknote/.quicknote_cache')
 	open(quicknote_cache, 'w+')
 
 		 
 """
 Makes the default note file
+and the archive note file
 """
-def make_default():
+def make_defaults():
 	default_file = path.expanduser('~/.quicknote/.default')
 	open(default_file, 'w+')
+	archive_file = path.expanduser('~/.quicknote/.archive')
+	open(archive_file, 'w+')
+
 
 def main():
 	args = sys.argv[1:]
 	if len(args) == 0:
 		make_memory_folder()
 		make_background_files()
-		make_default()
+		make_defaults()
 		print(BOLD + 'Quick Note' + RESET + ' installation successful')
 		print('Current note set to \'default\'')
 	else:
