8afa65793c61dd16a9ae950dffd9f01f3e1d7240
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 15:41:34 -0400

    Change download file in qnote.rb

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -1,7 +1,7 @@
 class Qnote < Formula
 	desc "A command line note-taking software"
 	homepage "https://github.com/scamacho23/quicknote"
-	url "https://github.com/scamacho23/homebrew-quicknote/blob/master/qnote-v1.0.6.tar.gz"
+	url "https://github.com/scamacho23/homebrew-quicknote/blob/master/qnote-v1.0.6.tar.xz"
 	sha256 "97cfb69948f11de2b93ec3e8653a90ad0c920eb582377543ad7f65450ce43fe7"
 	version "1.0.6"
 
