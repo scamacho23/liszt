58f1c84b40b711a8affa34c8dd908ada2c9cf462
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 15:42:51 -0400

    Change sha256 in qnote.rb

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -2,7 +2,7 @@ class Qnote < Formula
 	desc "A command line note-taking software"
 	homepage "https://github.com/scamacho23/quicknote"
 	url "https://github.com/scamacho23/homebrew-quicknote/blob/master/qnote-v1.0.6.tar.xz"
-	sha256 "97cfb69948f11de2b93ec3e8653a90ad0c920eb582377543ad7f65450ce43fe7"
+	sha256 "21d250f46775d9c966d6841949e8e3b28023710e1f14d82c3cbd8028d29cddc2"
 	version "1.0.6"
 
 	depends_on "python3"
