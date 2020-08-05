0f007fb72af2185583c12e4ddc61ce5c62ad9264
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 18:56:53 -0400

    Add new sha256 value to qnote.rb

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -2,13 +2,14 @@ class Qnote < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-quicknote"
   url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.8.tar.gz"
+  sha256 "4e578aeb0b2eeed06311ada45f5f90454b0add2d0b658d168947cc0c67911945"
   license "GNU GPL v3"
   version "1.0.6"
 
 
   def install
     bin.install "qnote"
-    Dir.mkdir("testing")
+    system Dir.mkdir("testing")
   end
 
 end
