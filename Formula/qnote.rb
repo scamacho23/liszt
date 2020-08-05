9324f7f53b7736723a334b4ac9aed47a2f05f4b5
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 19:22:25 -0400

    Change qnote.rb url

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -1,7 +1,7 @@
 class Qnote < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-quicknote"
-  url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.8.tar.gz"
+  url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.9.tar.gz"
   sha256 "4e578aeb0b2eeed06311ada45f5f90454b0add2d0b658d168947cc0c67911945"
   license "GNU GPL v3"
   version "1.0.6"
