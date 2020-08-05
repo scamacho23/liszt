ad8de7e47b83ff977c308714e1a97bff66762b2c
ebatelaan <56103979+ebatelaan@users.noreply.github.com>
2020-08-05 17:35:21 -0400

    Change qnote.rb file

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -1,17 +1,14 @@
 class Qnote < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-quicknote"
-  url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.6.tar.gz"
-  sha256 "f519eb3b14d698b87e74235a478e2b4d3d88eb7681738c805790bcfe23e58878"
+  url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.7.tar.gz"
   license "GNU GPL v3"
   version "1.0.6"
 
 
   def install
     bin.install "qnote"
+    echo "Hello There"
   end
 
-  test do
-    echo "hello there"
-  end
 end
