d24d77765f90f4cc8e6da21b43a88493d043136f
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 16:23:43 -0400

    Change qnote.rb file

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -1,16 +1,21 @@
-class Qnote < Formula
-	desc "A command line note-taking software"
-	homepage "https://github.com/scamacho23/quicknote"
-	url "https://github.com/scamacho23/homebrew-quicknote/blob/master/qnote-v1.0.6.zip"
-	sha256 "bc0431a49d865172b8d12216decf3c926018414723b18cfba50c51b80b80187e"
-	version "1.0.6"
+class qnote < Formula
+  desc "Software to save command line arguments as 'memories'"
+  homepage "https://github.com/scamacho23/homebrew-quicknote"
+  url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.6.tar.gz"
+  sha256 "f519eb3b14d698b87e74235a478e2b4d3d88eb7681738c805790bcfe23e58878"
+  license "GNU GPL v3"
+  version "1.0.6"
 
-	depends_on "python3"
-	
-	bottle :unneeded
 
-	def install
-		bin.install "qnote"
-		echo "Hello, World"
-	end
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    bin.install "qnote"
+  end
+
+  test do
+    echo "hello there"
+  end
 end
