468066f3b7bb7393599ee7c0cf3d56c763065198
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-05 19:23:00 -0400

    Remove sha256 val

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -2,7 +2,6 @@ class Qnote < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-quicknote"
   url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.9.tar.gz"
-  sha256 "4e578aeb0b2eeed06311ada45f5f90454b0add2d0b658d168947cc0c67911945"
   license "GNU GPL v3"
   version "1.0.6"
 
