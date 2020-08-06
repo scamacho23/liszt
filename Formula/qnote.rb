b933d3fd1b50f06e48b71ad991f06e0fb4299fb0
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-06 11:49:16 -0400

    Add sha256 value to qnote.rb

--- Formula/qnote.rb
+++ Formula/qnote.rb
@@ -2,6 +2,7 @@ class Qnote < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-quicknote"
   url "https://github.com/scamacho23/homebrew-quicknote/archive/v1.0.16.tar.gz"
+  sha256 "01c8049523e85fbb174771fff20fb56edcfc26f1e29c565396df9cb2ad59704a"
   license "GNU GPL v3"
   version "1.0.6"
 
