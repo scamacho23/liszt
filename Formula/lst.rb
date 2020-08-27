33926740bcafd96195418ff6ff1a4cf26401baa2
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-27 19:26:53 -0400

    Add bottle to lst.rb

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -5,6 +5,9 @@ class Lst < Formula
   license "GNU GPL v3"
   version "1.0.19"
 
+  bottle do
+    sha256 "175d8e525fbcbb59ca1953b402fdcf3714b5d1f8484ed80876b3f4f2377762e0" => :catalina  
+end
 
   def install
     bin.install "lst"
