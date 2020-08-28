16c2e872a5b8b7c9f6cb80b64e089e292eb79e33
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-27 23:33:26 -0400

    Update sha256 value

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -2,7 +2,7 @@ class Lst < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-liszt"
   url "https://github.com/scamacho23/homebrew-liszt/archive/v1.0.18.tar.gz"
-  sha256 "35ced0121ff589b41c0aa5278174047e23e41a57ad366dca2f19f4c331cf9c17"
+  sha256 "3f23d61a0f2fb49540ff284e426d7ab2deec3b56103e2bec8a901a37ef232481"
   license "GNU GPL v3"
   version "1.0.19"
 
