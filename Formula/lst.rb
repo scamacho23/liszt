8e5867982ef7051591d393bd8b6b69378f440874
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-27 23:44:56 -0400

    Add sha256 to lst.rb

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -2,6 +2,7 @@ class Lst < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-liszt"
   url "https://github.com/scamacho23/homebrew-liszt/archive/v1.0.20.tar.gz"
+  sha256 "bd5f661f4b0fa04d082562dbfa94931cf2ebd5b8a32e15ca22958216b9580754"
   license "GNU GPL v3"
   version "1.0.20"
 
