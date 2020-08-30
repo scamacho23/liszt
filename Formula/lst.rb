138b21033e4a26974946fe95b32f16204884579e
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-08-30 17:38:10 -0400

    Change url in lst.rb

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -1,9 +1,9 @@
 class Lst < Formula
   desc "Software to save command line arguments as 'memories'"
   homepage "https://github.com/scamacho23/homebrew-liszt"
-  url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.3.tar.gz"
+  url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.4.tar.gz"
   license "GNU GPL v3"
-  version "1.1.3"
+  version "1.1.4"
 
   def install
     system "autoconf"
