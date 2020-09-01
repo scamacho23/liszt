e6b396620978306a2be572ac6bc310e8b12b066c
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-01 13:55:35 -0400

    Add 1.1.7-alpha

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -1,10 +1,9 @@
 class Lst < Formula
   desc "Save snippets of information quickly through the command line"
   homepage "https://liszt.readthedocs.io"
-  url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.6.tar.gz"
-  sha256 "55f90361d00e5d579dadc44885b7cb984ece55dbe6ec66cb771174c7e6bf2adc"
+  url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.7-alpha.tar.gz"
   license "GNU GPL v3"
-  version "1.1.6"
+  version "1.1.7-alpha"
 
   depends_on "cmake" => :build
 
