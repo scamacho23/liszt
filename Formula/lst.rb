012bbcd432a6842d04d47289a3bf90548bae454b
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-01 14:38:09 -0400

    Add sha256 val to lst.rb

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -2,6 +2,7 @@ class Lst < Formula
   desc "Save snippets of information quickly through the command line"
   homepage "https://liszt.readthedocs.io"
   url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.7-alpha.tar.gz"
+  sha256 "30c9464e80e0b191a58471c0bd10ec6a054ae4fe668e61d1544302266b702af8"
   license "GNU GPL-3.0"
   version "1.1.7-alpha"
 
@@ -16,7 +17,7 @@ class Lst < Formula
   test do
     system "lst"
     expected = <<~EOS
-	"\x1B\[1mLiszt\ v1\.1\.7\-alpha\x1B\[0m\n"\n
+	"\e[1mLiszt v1.1.7-alpha\e[0m\n"
     EOS
 
     assert_match expected, shell_output("#{bin}/lst -v")
