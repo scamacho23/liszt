cbac01ab155810e364edc169da4c26e1832fb4c2
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-01 14:08:49 -0400

    Add test to lst.rb

--- Formula/lst.rb
+++ Formula/lst.rb
@@ -2,6 +2,7 @@ class Lst < Formula
   desc "Save snippets of information quickly through the command line"
   homepage "https://liszt.readthedocs.io"
   url "https://github.com/scamacho23/homebrew-liszt/archive/v1.1.7-alpha.tar.gz"
+  sha256 "30c9464e80e0b191a58471c0bd10ec6a054ae4fe668e61d1544302266b702af8"
   license "GNU GPL v3"
   version "1.1.7-alpha"
 
@@ -14,6 +15,10 @@ class Lst < Formula
   end
 
   test do
-    system "#{bin}/lst", "-h"
+    expected = <<~EOS
+      You have no memories at the moment.
+    EOS
+
+    assert_match expected, shell_output("#{bin}/lst -l")
   end
 end
